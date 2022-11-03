6adc011b25f0446343ca36d3610a8cfce124aabe
Ik <113038466+Ik1497@users.noreply.github.com>
2022-11-04 00:58:28 +0100

    Small Fixes

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <pageSettings
+  title="Streamer.bot Actions"
+  description="Everything you NEED for streaming!"/>
+</template>
+
+<script>
+import pageSettings from './components/home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    pageSettings
+  }
+}
+</script>
---
+++ src/components/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ title }}</h1>
+  <title>{{ title }} | Streamer.bot Actions</title>
+  <meta property="og:title" content="{{ title }}" />
+
+  <h2>{{ description }}</h2>
+  <meta property="og:description" content="{{ description }}" />
+
+  <meta name="theme-color" content="#B80086" />
+
+  <meta name="og:site_name" content="Streamer.bot Actions" />
+  <meta
+    property="og:image"
+    content="https://raw.githubusercontent.com/Ik1497/Ik1497/main/assets/profile_picture/Logo-Round.png"
+  />
+
+</template>
+
+<script>
+  export default {
+    name: "pageSettings",
+    props: {
+      title: String,
+      description: String,
+    },
+  };
+</script>
